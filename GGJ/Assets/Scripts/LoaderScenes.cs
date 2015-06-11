9a37f1a2d53307521d5990ac36fcb8c1a3838beb
KyTn <angel.kytn@gmail.com>
2015-06-11 13:29:19 +0200

    pantalla principal

--- GGJ/Assets/Scripts/LoaderScenes.cs
+++ GGJ/Assets/Scripts/LoaderScenes.cs
@@ -5,13 +5,13 @@ public class LoaderScenes : MonoBehaviour {
 
     public int p1 = 0;
     public int p2 = 0;
+    public GameObject g;
 
     public void load1P()
     {
-        Application.LoadLevel(p1);
-
+        //Application.LoadLevel(p1);
+        g.SetActive(true);
     }
-
     public void load2P()
     {
         Application.LoadLevel(p2);
