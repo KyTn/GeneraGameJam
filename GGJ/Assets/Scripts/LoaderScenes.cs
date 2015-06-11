2f21b6ff1e41ae8651643093ea0b5311d1e2c4af
KyTn <angel.kytn@gmail.com>
2015-06-11 13:00:10 +0200

    adsf

---
+++ GGJ/Assets/Scripts/LoaderScenes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoaderScenes : MonoBehaviour {
+
+    public int p1 = 0;
+    public int p2 = 0;
+
+    public void load1P()
+    {
+        Application.LoadLevel(p1);
+
+    }
+
+    public void load2P()
+    {
+        Application.LoadLevel(p2);
+
+    }
+}
